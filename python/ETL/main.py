be346bbe7041dcd0947675009100709d16e303b4
tom seufert <tseufert@gmail.com>
2025-01-22 12:33:36 -0500

    add job

--- python/ETL/main.py
+++ python/ETL/main.py
@@ -18,7 +18,7 @@ def main():
 
   # Step 1 - Load the Bronze Tables
   print("Step 1 .....")
-  table_name_bronze = "main.martingrund.taxi_demo_bronze"
+  table_name_bronze = "main.default.taxi_demo_bronze"
   df = trans.load_data(spark)
   trans.save_data(df, table_name_bronze)
 
@@ -29,15 +29,15 @@ def main():
   df = trans.transform_columns(df)
   df = trans.filter_invalid(df)
 
-  table_name_silver = "main.martingrund.taxi_demo_silver"
+  table_name_silver = "main.default.taxi_demo_silver"
   trans.save_data(df, table_name_silver)
 
   print(spark.table(table_name_silver).count())
 
   # Step 3 Prepare the views
   print("Step 3 .....")
-  yellow_view = "main.martingrund.taxi_demo_view_yellow"
-  green_view = "main.martingrund.taxi_demo_view_green"
+  yellow_view = "main.default.taxi_demo_view_yellow"
+  green_view = "main.default.taxi_demo_view_green"
   trans.create_view(spark, table_name_silver, yellow_view, 1)
   trans.create_view(spark, table_name_silver, green_view, 2)
 
